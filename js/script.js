1187507c479199bc4aba33d9943a1c241fdc4e5c
hn310 <hoangnam3101992@gmail.com>
2020-05-02 16:34:44 +0900

    homepage: footer left

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ $('#main-slider').carousel({
     pause: 'hover',
     wrap: true
 });
+
+
+// set company established year
+$("#company-year").text(getCompanyEstablisedYears());
+
+function getCompanyEstablisedYears() {
+    var establishedYear = 2004;
+    var currentYear = new Date().getFullYear();
+    return currentYear - establishedYear;
+}
+
+// set video url
+$(document).ready(function() {
+    var $videoSrc;
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    // when the modal is opened autoplay it  
+    $('#videoModal').on('shown.bs.modal', function(e) {
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    // stop playing the youtube video when I close the modal
+    $('#videoModal').on('hide.bs.modal', function(e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+    })
+    // document ready  
+});
